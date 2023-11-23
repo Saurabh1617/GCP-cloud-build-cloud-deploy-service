9c00bb57565c201eb9a80c6e6e6106fcbbb35929
Saurabh1617 <sourabhsharma1617@gmail.com>
2023-11-23 13:53:01 +0000

    test

--- 03-cb-cd-private-gke/gke_private_cluster.tf
+++ 03-cb-cd-private-gke/gke_private_cluster.tf
@@ -11,8 +11,8 @@ module "gke" {
   version         	     = "27.0.0"	
   project_id                 = var.target_project_id
   name                       = "gke-private-cluster"
-  region                     = "asia-south1"
-  zones                      = ["asia-south1-a", "asia-south1-b", "asia-south1-c"]
+  region                     = "us-central1"
+  zones                      = ["us-central1-a", "us-central1-b", "us-central1-c"]
   network                    = module.vpc.network_name
   subnetwork                 = module.vpc.subnets_names[0]
   ip_range_pods              = "pod-range"
@@ -32,9 +32,9 @@ module "gke" {
   }]
 
   node_pools = [{
-    name               = "default-node-pool" # Name for the Node Pool
+    name               = "my-node-pool" # Name for the Node Pool
     machine_type       = "e2-standard-4"     # Machine Type for Kubernetes Cluster
-    node_locations     = "asia-south1-a"     # Region for Node Locations. Must Match VPC region to provision
+    node_locations     = "us-central1-c"     # Region for Node Locations. Must Match VPC region to provision
     autoscaling        = true                # Enabling Auto Scaling for the Cluster
     auto_upgrade       = true                # Enabling Auto Upgrade Functionality
     initial_node_count = 1                   # Minimum Nodes required for ASM to work
